63ec765c08e856af650cdd0cb1dfceb6e55a4f6c
“jinxiansens” <hi@jinxiansen.com>
2016-05-21 16:10:20 +0800

    update README.

--- JPullDownMenuDemo/ViewController.m
+++ JPullDownMenuDemo/ViewController.m
@@ -24,9 +24,9 @@
     
     self.menu = [[JPullDownMenu alloc]initWithFrame:CGRectMake(0, 64,self.view.frame.size.width, 40) menuTitleArray:@[@"残剑",@"长空",@"飞雪"]];
     
-    NSArray * regionArray =@[@"黄浦区",@"南市区",@"卢湾区",@"徐汇区",@"长宁区",@"静安区",@"普陀区",@"闸北区",@"虹口区",@"杨浦区",@"闵行区",@"宝山区",@"嘉定区",@"浦东新区",@"金山区",@"松江区",@"奉贤区",@"青浦区",@"崇明区",@"不限"];
-    NSArray *classTypeArray=[[NSArray alloc]initWithObjects:@"学龄前",@"小学",@"初中",@"高中",@"成人",@"亲子班",@"一对一",@"小班",@"大班",@"长期",@"短期",@"次课", nil];
-    NSArray *sortRuleArray=[[NSArray alloc]initWithObjects:@"距离",@"价格",@"评分",@"最新",@"最热", nil];
+    NSArray * regionArray =@[@"黄浦区",@"南市区",@"卢湾区",@"徐汇区",@"不限"];
+    NSArray *classTypeArray=@[@"学龄前",@"小学",@"初中",@"高中",@"成人"];
+    NSArray *sortRuleArray=@[@"距离",@"价格",@"评分",@"最新",@"最热"];
     
     self.menu.menuDataArray = [NSMutableArray arrayWithObjects:regionArray, classTypeArray , sortRuleArray, nil];
     
@@ -34,8 +34,6 @@
     
     __weak typeof(self) _self = self;
     [self.menu setHandleSelectDataBlock:^(NSString *selectTitle, NSUInteger selectIndex, NSUInteger selectButtonTag) {
-        
-        NSLog(@"selectTitle = %@ selectIndex = @%lu selectButtonTag = @%lu",selectTitle,selectIndex,selectButtonTag);
         
         _self.titleLabel.text = [NSString stringWithFormat:@"selectTitle = %@\n selectIndex = @%lu\n selectButtonTag = @%lu",selectTitle,selectIndex,selectButtonTag];
     }];
@@ -45,9 +43,9 @@
 
 
 - (IBAction)changeData:(UIBarButtonItem *)sender {
-    NSArray * regionArray =@[@"奉贤区",@"青浦区",@"崇明区",@"不限",@"嘉定区",@"浦东新区",@"金山区"];
-    NSArray *classTypeArray=[[NSArray alloc]initWithObjects:@"短期",@"次课", nil];
-    NSArray *sortRuleArray=[[NSArray alloc]initWithObjects:@"价格",@"评分",@"最新",@"最热", nil];
+    NSArray * regionArray =@[@"不限",@"嘉定区",@"浦东新区",@"金山区"];
+    NSArray *classTypeArray=@[@"短期",@"长期"];
+    NSArray *sortRuleArray=@[@"价格",@"评分",@"最新",@"最热"];
    self.menu.menuDataArray = [NSMutableArray arrayWithObjects:regionArray, classTypeArray , sortRuleArray, nil];
     
     //数据源如果改变的话需调用此方法刷新数据。
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # JPullDownMenu
-This is a library of the drop-down menu .
+###这是一个简单的下拉菜单,使用详情见Demo。
+---
+
+![](image.png)
+
+## Usage
+
+```
+ self.menu = [[JPullDownMenu alloc]initWithFrame:CGRectMake(0, 64,self.view.frame.size.width, 40) menuTitleArray:@[@"残剑",@"长空",@"飞雪"]];
+ 
+    NSArray * regionArray =@[@"黄浦区",@"南市区",@"卢湾区",@"徐汇区",@"不限"];
+    NSArray *classTypeArray=@[@"学龄前",@"小学",@"初中",@"高中",@"成人"];
+    NSArray *sortRuleArray=@[@"距离",@"价格",@"评分",@"最新",@"最热"];
+    
+    self.menu.menuDataArray = [NSMutableArray arrayWithObjects:regionArray, classTypeArray , sortRuleArray, nil];
+    
+    [self.view addSubview:self.menu];
+    
+    __weak typeof(self) _self = self;
+    [self.menu setHandleSelectDataBlock:^(NSString *selectTitle, NSUInteger selectIndex, NSUInteger selectButtonTag) {
+        
+        _self.titleLabel.text = [NSString stringWithFormat:@"selectTitle = %@\n selectIndex = @%lu\n selectButtonTag = @%lu",selectTitle,selectIndex,selectButtonTag];
+    }];
+}
+```
+ 
+
+## Author
+
+jinxiansen, email: hi@jinxiansen.com
+
+## License
+
+JPullDownMenu is available under the (MIT)License.
